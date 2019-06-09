3bce3a00843843d3256b3a02aafb785c02eb16c5
erandro <erandro@gmail.com>
2019-06-09 13:43:55 -0500

    added compute and filter functions

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const app = new Vue({
     el: "#firstDiv",
     data: {
-        name: "eran",
-        age: 32
+        eran: {
+            first: "Eran",
+            last: "Dromy",
+            age: 32
+        },
+        natalie: {
+            first: "Natalie",
+            last: "Schneider",
+            age: 31
+        }
+    },
+    computed: {
+        eranFullName() {
+            return `${this.eran.first} ${this.eran.last}`
+        },
+        natalieFullName() {
+            return `${this.natalie.first} ${this.natalie.last}`
+        }
+    },
+    filters: {
+        fullName(val) {
+            return `${val.first} ${val.last}`
+        }
     },
     template: `
     <div>
-        <h1>Name: {{name}}</h1>
-        <h2>Age: {{age}}</h2>
+        <h3>Name: {{eran | fullName}}</h3>
+        <h4>Age: {{eran.age}}</h4>
+        <h3>Name: {{natalieFullName}}</h3>
+        <h4>Age: {{natalie.age}}</h4>
     </div>`
 })
