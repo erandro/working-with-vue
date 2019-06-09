365090c0de8daf09c5c1eeb04cdef5f3fc9776ac
erandro <erandro@gmail.com>
2019-06-09 17:38:37 -0500

    added directives and event

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
 const app = new Vue({
     el: "#firstDiv",
     data: {
-        eran: {
-            first: "Eran",
-            last: "Dromy",
-            age: 32
-        },
-        natalie: {
-            first: "Natalie",
-            last: "Schneider",
-            age: 31
-        }
-    },
-    computed: {
-        eranFullName() {
-            return `${this.eran.first} ${this.eran.last}`
-        },
-        natalieFullName() {
-            return `${this.natalie.first} ${this.natalie.last}`
-        }
+        residents: [
+            {
+                first: "Eran",
+                last: "Dromy",
+                age: 32
+            },
+            {
+                first: "Natalie",
+                last: "Schneider",
+                age: 31
+            }
+        ]
     },
     filters: {
         fullName(val) {
             return `${val.first} ${val.last}`
         }
     },
+    methods: {
+        incrementAge(name) {
+            name.age++
+        },
+        decrementAge(name) {
+            name.age++
+        }
+    },
     template: `
     <div>
-        <h3>Name: {{eran | fullName}}</h3>
-        <h4>Age: {{eran.age}}</h4>
-        <h3>Name: {{natalieFullName}}</h3>
-        <h4>Age: {{natalie.age}}</h4>
-    </div>`
+        <h2 v-for="resident in residents">
+        <h4>Name: {{resident | fullName}}</h4>
+        <h4>Age: {{resident.age}}</h4>
+        <button v-on:click="incrementAge(resident)">+</button>
+        <button v-on:click="decrementAge(resident)">-</button>
+        <input v-model="resident.first" />
+        <input v-model="resident.last" />
+        </h2>
+    </div>
+    `
+    // template: `
+    // <div>
+    //     <h3>Name: {{eran | fullName}}</h3>
+    //     <h4>Age: {{eran.age}}</h4>
+    //     <h3>Name: {{natalieFullName}}</h3>
+    //     <h4>Age: {{natalie.age}}</h4>
+    // </div>`
 })
