52bbd94ef798c7115bbbe59d8f2162bbaaf044fd
erandro <erandro@gmail.com>
2019-06-09 19:32:13 -0500

    added component logic

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+Vue.component("resident-component", {
+    props: ["resident"],
+    filters: {
+        fullName(val) {
+            return `${val.first} ${val.last}`
+        }
+    },
+    methods: {
+        incrementAge(name) {
+            name.age++
+        },
+        decrementAge(name) {
+            name.age++
+        }
+    },
+    template: `
+    <div>
+        <h4>Name: {{resident | fullName}}</h4>
+        <h4>Age: {{resident.age}}</h4>
+        <button v-on:click="incrementAge(resident)">+</button>
+        <button v-on:click="decrementAge(resident)">-</button>
+        <input v-model="resident.first" />
+        <input v-model="resident.last" />
+    </div>
+    `
+});
+
 const app = new Vue({
     el: "#firstDiv",
     data: {
@@ -14,36 +41,8 @@ const app = new Vue({
             }
         ]
     },
-    filters: {
-        fullName(val) {
-            return `${val.first} ${val.last}`
-        }
-    },
-    methods: {
-        incrementAge(name) {
-            name.age++
-        },
-        decrementAge(name) {
-            name.age++
-        }
-    },
     template: `
     <div>
-        <h2 v-for="resident in residents">
-        <h4>Name: {{resident | fullName}}</h4>
-        <h4>Age: {{resident.age}}</h4>
-        <button v-on:click="incrementAge(resident)">+</button>
-        <button v-on:click="decrementAge(resident)">-</button>
-        <input v-model="resident.first" />
-        <input v-model="resident.last" />
-        </h2>
-    </div>
-    `
-    // template: `
-    // <div>
-    //     <h3>Name: {{eran | fullName}}</h3>
-    //     <h4>Age: {{eran.age}}</h4>
-    //     <h3>Name: {{natalieFullName}}</h3>
-    //     <h4>Age: {{natalie.age}}</h4>
-    // </div>`
+        <resident-component v-for="item in residents" v-bind:resident="item" />
+    </div>`
 })
